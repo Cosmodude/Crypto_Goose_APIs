e2b8e399e2aee716f7056555ad6b85f04507df84
Hackdady <lenskii.vm19@physics.msu.ru>
2023-01-24 15:56:32 +0900

    added opensea API

--- External_API/CMC_API.py
+++ External_API/CMC_API.py
@@ -16,8 +16,9 @@ print(mydb)
 table_name='nft_pr'
 
 OpenSea_Url_Ending= {"Ice Poker":"decentral-games-ice","Stepn": "stepn","League of Kingdoms": "league-of-kingdoms" }
-quering=(f"SELECT id, nft_floor_price, daily_earn_rate_ET, required_token_name, earn_token_name, nft_required, name  FROM {table_name}; ")
+quering_all=(f"SELECT id, nft_floor_price, daily_earn_rate_ET, required_token_name, earn_token_name, nft_required, project_name  FROM {table_name}; ")
 updating=(f"UPDATE {table_name} SET nft_floor_price_D= %s, daily_earn_rate_D= %s, min_investment= %s WHERE id=%s ;")
+quering_name=(f"SELECT id, project_name FROM {table_name};")
 
 def get_from_db(db, process):
     cursor=db.cursor()
@@ -59,15 +60,26 @@ def OpenSea_API(project_name):
     return json
 
 def main():
-    db_response=get_from_db(mydb,quering)
+    db_response=get_from_db(mydb,quering_name)
+    for row in db_response:
+        if row[1] in OpenSea_Url_Ending.keys():
+            nft_price= OpenSea_API(OpenSea_Url_Ending[row[1]])["collection"]["stats"]["floor_price"]
+            cursor=mydb.cursor()
+            cursor.execute(f"UPDATE {table_name} SET nft_floor_price ={ nft_price } WHERE id={row[0]};")
+            mydb.commit() 
+            cursor.close()
+
+    db_response=get_from_db(mydb,quering_all)
     for row in db_response:
         required_token=CoinMarketCap_API(row[3])
         earn_token=CoinMarketCap_API(row[4])
         #print(CMC_response)
-        update_data=(\
-        #table_name,\
+        update_data=(
+        #nft_floor_price_D
         float(row[1])*required_token["data"][0]["quote"]["USD"]["price"],\
+        #daily_earn_rate_ET
         float(row[2])*earn_token["data"][0]["quote"]["USD"]["price"],\
+        #min_investment
         float(row[1])*required_token["data"][0]["quote"]["USD"]["price"]*float(row[5])*1.1,\
         row[0]
         )
