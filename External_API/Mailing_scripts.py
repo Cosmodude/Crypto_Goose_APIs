e20ae027476ff1c5d18d46a6930d5115cfc4554d
Hackdady <lenskii.vm19@physics.msu.ru>
2023-02-13 15:51:01 +0900

    changed the htmlbody source

--- External_API/Mailing_scripts.py
+++ External_API/Mailing_scripts.py
@@ -1,4 +1,5 @@
 ### Sending emails with the Postmark Python library
+import codecs
 from dotenv import load_dotenv
 load_dotenv()
 import os
@@ -10,20 +11,7 @@ def send_email(receiver_email,receiver_name):
         From='contact@crypto-goose.com',
         To=receiver_email,
         Subject='Welcome to Crypto Goose',
-        HtmlBody= f"<div className='email' style='\
-            border: 1px solid black;\
-            padding: 20px;\
-            font-family: sans-serif;\
-            line-height: 2;\
-            font-size: 20px;\
-            '>\
-            <h2> Hello, {receiver_name}!</h2>\
-            <p>Crypto Goose is currently focusing on working\
-             out the new features and will be giving updates based on your ideas.</p>\
-            <p>We might give you free subscriptions or airdrop\
-             out future tokens for our service when we finish our beta launch!<p>\
-            <p> Crypto Goose Team</p>\
-            </div>"\
+        HtmlBody= codecs.open("email_template.html", 'r').read()
     )
 #receiver_name = "bill"
 
